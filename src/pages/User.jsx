0eac886cf168d417ace5734dfff9056ab759cdae
Jishnu Mohan <jishnumohan292@gmail.com>
2024-02-25 17:52:39 +0530

    commit

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import Personal_Details from './resumes_content/Personal_Details'
-import Links from './resumes_content/Links'
-import Education from './resumes_content/Education'
-import Language from './resumes_content/Language'
-import Skill from './resumes_content/Skill'
+import React from "react";
+import Personal_Details from "./resumes_content/Personal_Details";
+import Links from "./resumes_content/Links";
+import Education from "./resumes_content/Education";
+import Language from "./resumes_content/Language";
+import Skill from "./resumes_content/Skill";
 
 function User() {
   return (
     <>
-    <Personal_Details/>
-    <Education/>
-    <Skill/>
-    <Language/>
-    <Links/>
+      <Personal_Details />
+      <Education />
+      <Skill />
+      <Language />
+      <Links />
     </>
-  )
+  );
 }
 
-export default User
+export default User;
